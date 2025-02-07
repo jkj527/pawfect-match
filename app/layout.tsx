8d3182e79cd0b3d62aceab993fef5641cfa32f5a
Jake Johnson <jkjohnson1996@gmail.com>
2025-02-06 18:19:05 -0800

    built file structure, including components, search home page, individual dog details page, auth login page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Montserrat_Alternates } from "next/font/google";
-import "./globals.css";
+import "./styles/globals.css";
 
 const montserrat =  Montserrat_Alternates({
   subsets: ["latin"],
@@ -13,11 +13,7 @@ export const metadata: Metadata = {
   description: "Find your perfect pet partner!",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={montserrat.variable}>
       <body>
